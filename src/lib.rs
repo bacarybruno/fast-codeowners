8366c62b1fe7cf9c35221113d3e33a835cc80363
bacarybruno <brunobacary.bodian@doctolib.com>
2025-06-16 09:34:13 +0200

    chore: first implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,60 @@
-#![deny(clippy::all)]
+use codeowners_rs::{parse, parse_file, RuleSet};
+use napi::Result;
+use std::path::PathBuf;
 
 use napi_derive::napi;
 
 #[napi]
-pub fn plus_100(input: u32) -> u32 {
-  input + 100
+pub struct Codeowners {
+  ruleset: RuleSet,
+  excluded_owners: Vec<String>,
+}
+
+#[napi]
+impl Codeowners {
+  #[napi(constructor)]
+  pub fn new(excluded_owners: Option<Vec<String>>) -> Result<Self> {
+    let ruleset = Self::create_ruleset()?;
+    
+    Ok(Codeowners { ruleset, excluded_owners: excluded_owners.unwrap_or_default() })
+  }
+
+  #[napi(factory)]
+  pub fn from_content(content: String, excluded_owners: Option<Vec<String>>) -> Result<Self> {
+    let ruleset = Self::create_ruleset_from_content(content)?;
+    Ok(Codeowners { ruleset, excluded_owners: excluded_owners.unwrap_or_default() })
+  }
+
+  #[napi]
+  pub fn get_owners(&self, path: String) -> Vec<String> {
+    self.ruleset.owners(&path)
+      .unwrap_or_default()
+      .iter()
+      .map(|owner| owner.value.clone())
+      .filter(|owner| !self.excluded_owners.contains(owner))
+      .collect()
+  }
+
+  fn codeowners_path() -> Option<PathBuf> {
+    const DEFAULT_PATHS: &'static [&'static str] = &[".github/CODEOWNERS", ".gitlab/CODEOWNERS", "docs/CODEOWNERS", "CODEOWNERS"];
+  
+    DEFAULT_PATHS
+            .iter()
+            .map(PathBuf::from)
+            .find(|p| p.exists())
+  }
+  
+  fn create_ruleset() -> Result<RuleSet> {
+    let codeowners_path = Self::codeowners_path()
+      .ok_or_else(|| napi::Error::new(napi::Status::GenericFailure, "No CODEOWNERS file found"))?;
+    let ruleset = parse_file(&codeowners_path)
+      .map_err(|e| napi::Error::new(napi::Status::GenericFailure, format!("Failed to parse CODEOWNERS: {}", e)))?
+      .into_ruleset();
+    Ok(ruleset)
+  }
+  
+  fn create_ruleset_from_content(content: String) -> Result<RuleSet> {
+    let ruleset = parse(&content).into_ruleset();
+    Ok(ruleset)
+  }
 }
