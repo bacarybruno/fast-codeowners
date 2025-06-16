572e7a566f58356f5746d19543cc8963987063b8
bacarybruno <brunobacary.bodian@doctolib.com>
2025-06-16 09:46:19 +0200

    chore: cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -15,19 +15,27 @@ impl Codeowners {
   #[napi(constructor)]
   pub fn new(excluded_owners: Option<Vec<String>>) -> Result<Self> {
     let ruleset = Self::create_ruleset()?;
-    
-    Ok(Codeowners { ruleset, excluded_owners: excluded_owners.unwrap_or_default() })
+
+    Ok(Codeowners {
+      ruleset,
+      excluded_owners: excluded_owners.unwrap_or_default(),
+    })
   }
 
   #[napi(factory)]
   pub fn from_content(content: String, excluded_owners: Option<Vec<String>>) -> Result<Self> {
     let ruleset = Self::create_ruleset_from_content(content)?;
-    Ok(Codeowners { ruleset, excluded_owners: excluded_owners.unwrap_or_default() })
+    Ok(Codeowners {
+      ruleset,
+      excluded_owners: excluded_owners.unwrap_or_default(),
+    })
   }
 
   #[napi]
   pub fn get_owners(&self, path: String) -> Vec<String> {
-    self.ruleset.owners(&path)
+    self
+      .ruleset
+      .owners(&path)
       .unwrap_or_default()
       .iter()
       .map(|owner| owner.value.clone())
@@ -36,23 +44,30 @@ impl Codeowners {
   }
 
   fn codeowners_path() -> Option<PathBuf> {
-    const DEFAULT_PATHS: &'static [&'static str] = &[".github/CODEOWNERS", ".gitlab/CODEOWNERS", "docs/CODEOWNERS", "CODEOWNERS"];
-  
-    DEFAULT_PATHS
-            .iter()
-            .map(PathBuf::from)
-            .find(|p| p.exists())
+    const DEFAULT_PATHS: &'static [&'static str] = &[
+      ".github/CODEOWNERS",
+      ".gitlab/CODEOWNERS",
+      "docs/CODEOWNERS",
+      "CODEOWNERS",
+    ];
+
+    DEFAULT_PATHS.iter().map(PathBuf::from).find(|p| p.exists())
   }
-  
+
   fn create_ruleset() -> Result<RuleSet> {
     let codeowners_path = Self::codeowners_path()
       .ok_or_else(|| napi::Error::new(napi::Status::GenericFailure, "No CODEOWNERS file found"))?;
     let ruleset = parse_file(&codeowners_path)
-      .map_err(|e| napi::Error::new(napi::Status::GenericFailure, format!("Failed to parse CODEOWNERS: {}", e)))?
+      .map_err(|e| {
+        napi::Error::new(
+          napi::Status::GenericFailure,
+          format!("Failed to parse CODEOWNERS: {}", e),
+        )
+      })?
       .into_ruleset();
     Ok(ruleset)
   }
-  
+
   fn create_ruleset_from_content(content: String) -> Result<RuleSet> {
     let ruleset = parse(&content).into_ruleset();
     Ok(ruleset)
